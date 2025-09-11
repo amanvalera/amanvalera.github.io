ded2f16312e1d76382d7cbf00c8c5908a1165166
amanvalera <43451649+amanvalera@users.noreply.github.com>
2025-09-11 16:37:43 +0100

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,6 +1,6 @@
 var addressPoints = [
   [
-    "Cricket Guru \u2013 IPL Match Outcome Prediction<br />Heriot-Watt University Student Showcase 2024; Edinburgh, UK",
+    "Cricket Guru \u2013 IPL Match Outcome Prediction<br />Heriot-Watt University; Edinburgh, UK",
     55.9533456,
     -3.1883749
   ]
